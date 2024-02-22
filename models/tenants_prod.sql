2bb0624800b2b6722a4046560feaa78a14e21186
Bibin Venugopal <mails2bibin@gmail.com>
2024-02-22 22:18:50 +0530

    Updated the model

--- models/tenants_prod.sql
+++ models/tenants_prod.sql
@@ -1,6 +1,29 @@
+-- WITH source AS (
+--    SELECT
+      --  alias_name,
+      --   created_at,
+      --   created_by_admin_user_id,
+      --   created_by_user_id,
+      --   deleted_at,
+      --   hq_flag,
+      --   hq_id,
+      --   id,
+      --   managed_by_id,
+      --   name,
+      --   non_participating,
+      --   subdomain,
+      --   cast(tenant_types as text) as tenant_types,
+      --   test_tenant,
+      --   updated_at
+   -- FROM public.monolith_tenants
+-- )
+
+-- select * from source
+{{ config(materialized='incremental', unique_key='id') }}
+
 WITH source AS (
-   SELECT
-       alias_name,
+    SELECT
+        alias_name,
         created_at,
         created_by_admin_user_id,
         created_by_user_id,
@@ -15,7 +38,11 @@ WITH source AS (
         cast(tenant_types as text) as tenant_types,
         test_tenant,
         updated_at
-   FROM public.monolith_tenants
+    FROM {{ source('my_source', 'monolith_tenants') }}
 )
 
 select * from source
+
+{% if is_incremental() %}
+  WHERE updated_at > (SELECT max(updated_at) FROM {{ this }})
+{% endif %}
